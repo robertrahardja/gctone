54801c0adb6663f5b0278179c42b13f10261951f
robertrahardja <robertrahardja@gmail.com>
2025-06-23 14:31:18 +0800

    created phase 3: cdk project structure

---
+++ test/gctone.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Gctone from '../lib/gctone-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/gctone-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Gctone.GctoneStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
